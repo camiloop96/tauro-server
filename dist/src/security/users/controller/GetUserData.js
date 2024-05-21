19cb676e068cae386b6a6175c4f7de787b8f5073
Camilo Polania <cpolania@outlook.com>
2024-05-21 10:35:11 -0500

    Deploy

--- dist/src/security/users/controller/GetUserData.js
+++ dist/src/security/users/controller/GetUserData.js
@@ -18,6 +18,7 @@ const UserModel_1 = __importDefault(require("../models/UserModel"));
 const dateManager_1 = require("../../../utils/dateManager");
 const EmployeeModel_1 = require("../../../staff/Employee/models/EmployeeModel");
 const BranchModel_1 = require("../../../store/branch/models/BranchModel");
+const SellerModel_1 = require("../../../staff/Seller/models/SellerModel");
 const GetUserData = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     console.log(`${(0, dateManager_1.getCurrentDate)()} ${req.method} simora/api/authentication/security/user/data/by-token`);
     let { token } = req.body;
@@ -29,19 +30,27 @@ const GetUserData = (req, res) => __awaiter(void 0, void 0, void 0, function* ()
                 error: "No hay usuario asociado",
             });
         }
+        // Validacion de empleado
         let findEmployee = yield EmployeeModel_1.EmployeeModel.findById(user === null || user === void 0 ? void 0 : user.employee);
-        let userData;
-        if (findEmployee !== null) {
-            let findBranchStore = yield BranchModel_1.BranchStoreModel.findById(findEmployee === null || findEmployee === void 0 ? void 0 : findEmployee.branchStore);
-            userData = {
-                _id: findEmployee === null || findEmployee === void 0 ? void 0 : findEmployee._id,
-                fullName: `${findEmployee === null || findEmployee === void 0 ? void 0 : findEmployee.name} ${findEmployee === null || findEmployee === void 0 ? void 0 : findEmployee.lastName}`,
-                cityBranchStore: {
-                    name: findBranchStore === null || findBranchStore === void 0 ? void 0 : findBranchStore.name,
-                    city: findBranchStore === null || findBranchStore === void 0 ? void 0 : findBranchStore.city,
-                },
-            };
+        if (!findEmployee) {
+            return res.status(400).json({
+                error: "No hay empleado asociado",
+            });
         }
+        // Busqueda de Vendedor y Sucursal
+        let findBranchStore = yield BranchModel_1.BranchStoreModel.findById(findEmployee === null || findEmployee === void 0 ? void 0 : findEmployee.branchStore);
+        let findSeller = yield SellerModel_1.SellerModel.findOne({
+            employee: findEmployee._id,
+        });
+        let userData = {
+            _id: findEmployee === null || findEmployee === void 0 ? void 0 : findEmployee._id,
+            fullName: `${findEmployee === null || findEmployee === void 0 ? void 0 : findEmployee.name} ${findEmployee === null || findEmployee === void 0 ? void 0 : findEmployee.lastName}`,
+            cityBranchStore: {
+                name: findBranchStore === null || findBranchStore === void 0 ? void 0 : findBranchStore.name,
+                city: findBranchStore === null || findBranchStore === void 0 ? void 0 : findBranchStore.city,
+            },
+            sellerID: findSeller ? findSeller === null || findSeller === void 0 ? void 0 : findSeller._id : null,
+        };
         return res.status(200).json(userData);
     }
     catch (error) {
