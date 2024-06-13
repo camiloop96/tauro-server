d98f449012a0cf919b88bd2abe17b1f7a77f1b05
Camilo Polania <cpolania@outlook.com>
2024-06-13 11:44:33 -0500

    doc(AppError): Documentation was added for AppError component

--- dist/shared/errors/AppError.js
+++ dist/shared/errors/AppError.js
@@ -1,8 +1,22 @@
 "use strict";
+/**
+ * @file AppError.ts
+ * @description Custom error class for application-specific errors. Extends the built-in `Error` class to include additional properties such as `statusCode` and `originalError`.
+ */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppError = void 0;
-// src/shared/errors/AppError.ts
+/**
+ * Class representing an application-specific error.
+ * @extends Error
+ */
 class AppError extends Error {
+    /**
+     * Creates an instance of `AppError`.
+     *
+     * @param {string} message - A descriptive error message.
+     * @param {number} statusCode - The HTTP status code associated with the error.
+     * @param {any} [originalError] - An optional parameter to include the original error object.
+     */
     constructor(message, statusCode, originalError) {
         super(message);
         this.message = message;
--- src/shared/errors/AppError.ts
+++ src/shared/errors/AppError.ts
@@ -1,5 +1,20 @@
-// src/shared/errors/AppError.ts
+/**
+ * @file AppError.ts
+ * @description Custom error class for application-specific errors. Extends the built-in `Error` class to include additional properties such as `statusCode` and `originalError`.
+ */
+
+/**
+ * Class representing an application-specific error.
+ * @extends Error
+ */
 export class AppError extends Error {
+  /**
+   * Creates an instance of `AppError`.
+   *
+   * @param {string} message - A descriptive error message.
+   * @param {number} statusCode - The HTTP status code associated with the error.
+   * @param {any} [originalError] - An optional parameter to include the original error object.
+   */
   constructor(
     public message: string,
     public statusCode: number,
